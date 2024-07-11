b45acafd911c96c51569d0ad36316760a05bbb2d
Lyu Ji <hkfhfkcoc@protonmail.com>
2024-07-10 19:59:52 -0700

    add sort navigation bar and products (#1)
    Co-authored-by: ivy <j.song1@student.fdu.edu>

---
+++ Pages/NewArrivals.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.Pages
+{
+    public class NewArrivalsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
